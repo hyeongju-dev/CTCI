c2f206052f21fdef8f3f30614d032b41a736ebec
hyeongju-dev <xxox71@gmail.com>
2020-09-26 14:21:25 +0900

    solve follow-up problem: 'what if the elements are not distinct?'

--- 8.3.cpp
+++ 8.3.cpp
@@ -1,19 +1,38 @@
 #include <vector>
 using namespace std;
 
-int magicFast(vector<int> v) {
+int magicFast(vector<int>  v) {
   return magicFast(v, 0, v.size()-1);
 }
 
-int magicFast(vector<int> &v, int start, int end) {
-  if (end<start)
+int magicFast(const vector<int> &v, int start, int end) {
+  if (end < start)
     return -1;
 
-  int mid = start+(end-start)/2;
+  int mid = start + (end-start)/2;  // to avoid overflow. effectively, equals (start+end)/2
   if (v[mid] == mid)
     return mid;
   else if (v[mid] > mid)
     return magicFast(v, start, mid-1);
   else
     return magicFast(v, mid+1, end);
+}
+
+/* FOLLOW-UP: What if the elements are not distinct? */
+int magicFast_followup(const vector<int> &v, int start, int end) {
+  if (end < start)
+    return -1;
+
+  int mid = start + (end-start)/2;
+  if (v[mid] == mid)
+    return mid;
+
+  int left = (mid-1 < v[mid] ? mid-1 : v[mid]);
+  int leftValue = magicFast(v, start, left);
+  if (leftValue >= 0)
+    return leftValue;
+
+  int right = (mid+1 > v[mid] ? mid+1 : v[mid]);
+  int rightValue = magicFast(right, end);
+  return right;
 }
