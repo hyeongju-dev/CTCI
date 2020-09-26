858e289d6f8d09d34c36f027c31314ada9e84290
hyeongju-dev <xxox71@gmail.com>
2020-09-26 11:44:57 +0900

    correct minor error in the recursive method that I had previously written and write an iterative way to solve this dp problem

--- 8.1.cpp
+++ 8.1.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int countWays(int n) {
-  vector<int> cache(n,-1);
+  vector<int> cache(n, -1);
   return countWays(n, cache);
 }
 
-int countWays(int n, vector<int> &cache) {
-  if (n < 1) {
+int countWays_Recursive(int n, vector<int> &cache) {
+  if (n < 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n == 0) {
     return 1;
   } else if (cache[n] > -1) {
     return cache[n];
@@ -17,4 +17,11 @@ int countWays(int n, vector<int> &cache) {
     cache[n] = countWays(n-1, cache) + countWays(n-2, cache) + countWays(n-3, cache);
     return cache[n];
   }
+}
+
+int countWays_Iterative(int n, vector<int> &cache) {
+  cache[1] = 1; cache[2] = 2; cache[3] = 4;
+  for (int i = 4; i <= n; ++i)
+    cache[i] = cache[i-1] + cache[i-2] + cache[i-3];
+  return cache[n];
 }
