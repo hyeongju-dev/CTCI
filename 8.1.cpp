1b1eb1862d20cae453f055d87547f258ffdb50f4
hyeongju-dev <xxox71@gmail.com>
2020-09-26 11:19:49 +0900

    rename the variable 'memo' to 'cache' for intuition

--- 8.1.cpp
+++ 8.1.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int countWays(int n) {
-  vector<int> memo(n,-1);
-  return countWays(n, memo);
+  vector<int> cache(n,-1);
+  return countWays(n, cache);
 }
 
-int countWays(int n, vector<int> &memo) {
-  if (n<1)
+int countWays(int n, vector<int> &cache) {
+  if (n < 1) {
     return 0;
-  else if (n == 1)
+  } else if (n == 1) {
     return 1;
-  else if (memo[n] > -1)
-    return memo[n];
-  else {
-    memo[n] = countWays(n-1,memo) + countWays(n-2,memo) + countWays(n-3,memo);
-    return memo[n];
+  } else if (cache[n] > -1) {
+    return cache[n];
+  } else {
+    cache[n] = countWays(n-1, cache) + countWays(n-2, cache) + countWays(n-3, cache);
+    return cache[n];
   }
 }
